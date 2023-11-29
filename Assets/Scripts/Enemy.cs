37e7f5cf960549f36389d6f551369ddd5b6efbd8
llln248 <134625592+llln248@users.noreply.github.com>
2023-11-29 13:03:15 -0600

    score works!

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,29 +4,28 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public int maxHealth;
+    public int maxHealth = 20;
     int currentHealth;
     public GameObject obj;
-    //public GameObject display;
-    //public UIInformation info;
+    public GameObject HUD;
+    public UIInformation info;
     // Start is called before the first frame update
     void Start()
     {
-        //currentHealth = maxHealth;
-        //info = display.GetComponent<UIInformation>();
+        currentHealth = maxHealth;
+        info = HUD.GetComponent<UIInformation>();
     }
 
     public void takeDamage()
     {
-        currentHealth--;
+        currentHealth = currentHealth - 20;
         if (currentHealth <= 0)
             Die();
     }
-    void Die()
-    {
-    GetComponent<Collider2D>().enabled = false;
-    //this.enabled = false;
-    obj.SetActive(false);
-    //info.updateScore(100);
+    void Die() {
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+        obj.SetActive(false);
+        info.updateScore(100);
     }
 }
